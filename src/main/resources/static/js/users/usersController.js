c8189d0ec64afb8fe50554fea192e921c756d05a
Aaron Arce Hernandez <aaronarce02@gmail.com>
2017-01-25 17:43:54 -0600

    Add fonts files

--- src/main/resources/static/js/users/usersController.js
+++ src/main/resources/static/js/users/usersController.js
@@ -1,8 +1,21 @@
-angular.module('users')
-	.controller('UsersController', UsersController);
+angular.module('users').controller('UsersController', UsersController);
 
 function UsersController() {
 	var vm = this;
-	
-	
+
+	vm.users = [ {
+		email: 'aaronarce02@gmail.com',
+		firstname : 'Aaron',
+		lastname : 'Arce'
+	},
+	{
+		email: 'johnsmith@gmail.com',
+		firstname : 'John',
+		lastname : 'Smith'
+	},
+	{
+		email: 'mrodgers@gmail.com',
+		firstname : 'Michael',
+		lastname : 'Rodgers'
+	}];
 }
